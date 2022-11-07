225c2cd34eb350ec8505200a4e241f3ccb2c3742
c4ibD3 <280206014@qq.com>
2022-11-07 17:52:53 +0800

    计算器界面的基本UI布局
    计算器界面的基本UI布局

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 张鹏 on 2022/11/7.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
